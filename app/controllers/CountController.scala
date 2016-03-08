b4d8c5be4862d8d04f81273f2e963d238eaceef5
knoldus <mehrotrasonu2@gmail.com>
2016-03-08 14:26:42 +0530

    demo

--- app/controllers/CountController.scala
+++ app/controllers/CountController.scala
@@ -7,7 +7,7 @@ import play.api.mvc._
 import services.Counter
 
 /**
- * This controller demonstrates how to use dependency injection to
+ * This controller how to use dependency injection to
  * bind a component into a controller class. The class creates an
  * `Action` that shows an incrementing count to users. The [[Counter]]
  * object is injected by the Guice dependency injection system.
